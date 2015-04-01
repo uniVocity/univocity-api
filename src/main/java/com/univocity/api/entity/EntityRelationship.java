9d9b81433b589e809c8ceff936293ab55e28fd25
Jeronimo Backes <jbax@univocity.com>
2015-04-01 18:07:20 +1030

    implementing toString() method for EntityRelationship

--- src/main/java/com/univocity/api/entity/EntityRelationship.java
+++ src/main/java/com/univocity/api/entity/EntityRelationship.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public abstract class EntityRelationship {
 
+	private String myStringRepresentation = null;
+
 	public abstract Entity getEntity();
 
 	public abstract List<DefaultEntityField> getFields();
@@ -11,4 +13,27 @@ public abstract class EntityRelationship {
 	public abstract Entity getReferredEntity();
 
 	public abstract List<DefaultEntityField> getReferredFields();
+
+	public String toString() {
+		if (myStringRepresentation == null) {
+			myStringRepresentation = printEntityAndFields(getEntity(), getFields()) + "->" + printEntityAndFields(getReferredEntity(), getReferredFields());
+		}
+		return myStringRepresentation;
+	}
+
+	private String printEntityAndFields(Entity entity, List<DefaultEntityField> fields) {
+		StringBuilder tmp = new StringBuilder();
+
+		for (DefaultEntityField field : fields) {
+			if (tmp.length() > 0) {
+				tmp.append(',');
+			}
+			tmp.append(field.getName());
+		}
+		tmp.insert(0, '[');
+		tmp.insert(0, entity.getEntityName());
+		tmp.append(']');
+
+		return tmp.toString();
+	}
 }
