a03baa9d404e7587c0bed1fcc514b211842422c7
Jeronimo Backes <jbax@univocity.com>
2015-06-02 00:34:52 +0930

    adding FieldIdentifiers to value replacement API

--- src/main/java/com/univocity/api/config/builders/ReplaceValues.java
+++ src/main/java/com/univocity/api/config/builders/ReplaceValues.java
@@ -1,8 +1,14 @@
 package com.univocity.api.config.builders;
 
+import com.univocity.api.entity.*;
+
 public interface ReplaceValues {
 
 	public ValueReplacement fromFields(String... fieldNames);
 
+	public ValueReplacement fromFields(FieldIdentifier... fieldIdentifiers);
+
 	public UnmatchedReference associatedWith(String sourceEntity, String sourceFieldName);
+
+	public UnmatchedReference associatedWith(String sourceEntity, FieldIdentifier fieldIdentifier);
 }
