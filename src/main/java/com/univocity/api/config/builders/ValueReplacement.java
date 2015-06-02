6b84df433bccc78f1a5b71590bd76e4510754475
Jeronimo Backes <jbax@univocity.com>
2015-06-02 19:37:16 +0930

    adding support for FieldIdentifier in the value replacement API

--- src/main/java/com/univocity/api/config/builders/ValueReplacement.java
+++ src/main/java/com/univocity/api/config/builders/ValueReplacement.java
@@ -2,11 +2,15 @@ package com.univocity.api.config.builders;
 
 import java.util.*;
 
+import com.univocity.api.entity.*;
+
 
 public interface ValueReplacement {
 	
 	public UnmatchedReference with(Map<?,?> valueMap);
 
 	public UnmatchedReference with(String entityName, String keyField, String valueField);
+	
+	public UnmatchedReference with(String entityName, FieldIdentifier keyField, FieldIdentifier valueField);
 }
 
